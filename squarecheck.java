90573ddfce15f0ace1603785abca661eabfb9610
Willyfsg <56430375+Willyfsg@users.noreply.github.com>
2020-10-03 20:45:54 +0200

    Update and rename ejercicio8.java to squarecheck.java

---
+++ squarecheck.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+
+public class squarecheck {
+	
+	public static void main (String arg[]) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		double num, check;
+		
+		System.out.println("Welcome to the Program");
+		System.out.print("Input a number to see if there is a square of it: ");
+		
+		num = input.nextInt();
+		check = Math.sqrt(num) ; 
+		
+		if (num % check == 0) {
+			System.out.println("Yeah, has a square number!");
+		}else {
+			System.out.println("No, doesn't have a square number!.");
+
+		}
+		
+		
+	}
+
+
+}
