75f5b3d6600c4c0a40adfe96ca70d9d7fcf032a1
Willyfsg <56430375+Willyfsg@users.noreply.github.com>
2020-10-03 20:43:57 +0200

    Update and rename ejercicio6.java to module.java

---
+++ module.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class module {
+	public static void main(String arg[]) {
+		
+		Scanner input = new Scanner(System.in);
+		int num, extra, b;
+		
+		System.out.println("Welcome to the Program\n");
+		System.out.print("Input a number: ");
+		num = input.nextInt();
+		
+		num = num * 20;
+		extra = num/10;
+		System.out.println("The number multiply by twenty and divided by ten is: " + extra);
+		
+		extra = extra + num;
+		
+		System.out.println("The number add to the multiply: " + extra);
+		
+		b = extra/10 % 2;
+		
+		if (b==0) {
+			System.out.println("The result from the division has residual zero: " + extra/10);
+		}else {
+			System.out.println("The result from the division has NOT residual zero: " + extra/10);
+		}
+
+		
+		System.out.println("\n\nEnd of Program.");
+		
+	}
+}
