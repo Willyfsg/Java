64350c85f7eb08fbf791e8fd60361cb3c45ded5c
Willyfsg <56430375+Willyfsg@users.noreply.github.com>
2020-10-03 20:54:01 +0200

    Update and rename ejercicio7.java to oddpaircheck.java

---
+++ oddpaircheck.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class oddpaircheck {
+	
+	public static void main(String arg[]) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int num,b;
+		
+		System.out.println("The Program Starts");
+		System.out.print("Input a number to check if is ODD or PAIR: ");
+
+		num = input.nextInt();
+		
+		b = num % 2;
+		if (b!=0) {
+			System.out.println("\nIts a ODD number.");
+		}else {
+			System.out.println("\nIts a PAIR number.");
+		}
+		
+		System.out.println("\n\nEnd of Program.");
+
+		
+		
+	}
+
+}
